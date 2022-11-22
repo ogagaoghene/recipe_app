02508f6348b83142ba669e0fefaa4de78454da87
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-22 22:14:35 +0100

    Update Devise User with confirmable module

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_scope :users do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
   devise_for :users
   root to: 'users#index'
   resources :recipe_foods
