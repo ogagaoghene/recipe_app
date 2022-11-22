0753b7fc732a58f97d0caf51338297ffce8bfe38
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-22 21:21:24 +0100

    Fix linting errors for application_controller.rb, users_controller.rb and user.rb files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,4 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   has_many :foods
   has_many :recipes
   validates :name, presence: true
-       
 end
